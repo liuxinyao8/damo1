a624b71962da96c3f6a4d4e7759723f4e6cc3203
liuxinyao8 <2481089336@qq.com>
2019-04-29 20:25:35 +0800

    11111111223

--- app/src/main/java/com/example/lenovo/day01/MainActivity.java
+++ app/src/main/java/com/example/lenovo/day01/MainActivity.java
@@ -1,18 +1,40 @@
 package com.example.lenovo.day01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+    private ArrayList<Fragment> fragments;
+    /**
+     * 哇哇哇哇
+     */
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         initdata();
     }
 
     private void initdata() {
-        
+        fragments = new ArrayList<>();
+        fragments.add(new Fragment1());
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments);
+        vp.setAdapter(fragmentAdapter);
+    }
+
+    private void initView() {
+        vp = (ViewPager) findViewById(R.id.vp);
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText("111111112315646416");
     }
 }
